bd00a13d88cdc814e5bc0fe223ea527e448458b9
Parvin Sattorova <nanajon66@gmail.com>
2022-12-08 16:36:05 -0500

    Add the answersto problems

--- for.js
+++ for.js
@@ -3,13 +3,22 @@
   
 // Challenge 1
 // Use a for statement to log to the console all even numbers between 25 and 75.
-
-
+```
+for (var i = 26; i <= 74; i+=2) {
+  console.log(i)
+}
+```
 
 // Challenge 2
 // Use a for statement to log to the console a complete sentence stating the weather using the Array below:
+//var weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
+```
 var weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
 
+for(var i = 0; i < weather_options.length; i++){
+  console.log("Weather can be:" + weather_options [i])
+}
+```
 
 // Challenge 3
 // Use a for statement to log to the console only the words that are exactly 3 characters in length
